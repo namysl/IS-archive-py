26cba37b7671c920b9e5ae36c57909831a7bb203
namysl <e.k.namysl@gmail.com>
2021-11-04 14:55:03 +0100

    ex 8, ex 18

--- C/zajecia2/8.C
+++ C/zajecia2/8.C
@@ -1,31 +1,63 @@
 #include <stdio.h>
 
+char gwiazdka=42;
+char spacja=32;
+int n=0;
+
+int main() {
+    printf("Podaj n: ");
+    scanf("%d", &n);
+
+	for (int i=2; i<=n; i++) {
+		for (int j=1; j<=i; j++) {
+			for (int k=0; k<(n-j); k++){
+                putchar(spacja);
+			}
+            for (int p=0; p<(j*2-1); p++){
+                putchar(gwiazdka);
+            }
+            printf("\n");
+		}
+	}
+
+    return 0;
+}
+
+/*#include <stdio.h>
+
 int n; // ilosc segmentow
 int k; // numer segmentu -> 1, ..., n
 int ik; // i-ty wiersz w segmencie
-char spacja = ' ';
-char gwiazdka = '*';
-/*
+char spacja = 32;
+char gwiazdka = 42;
 
+/*
    *
   ***
    *
   ***
  *****
-
 */
 
+/*
 int main(){
+    printf("Podaj n: ");
+    scanf("%i\n", &n);
 
-    scanf("%i\n", n);
-
-    for (k=1; k<=n; k++){
-        for (ik=1; ik<=k+1; k++){
+    for (k=2; k<=n; k++){
+        for (ik=1; ik<=k; k++){
+            for (int q=0; q<=n+1-ik; q++){
+                putchar(spacja);
+            }
+            for (int w=0; w<=2*ik-1; w++){
+                putchar(gwiazdka);
+            }
             // n+1-ik spacji + 2*ik-1 gwiazdek
-            putchar('%c%c', spacja, gwiazdka);
-            printf('\n');
+
+            printf("\n");
         }
     }
 
     return 0;
 }
+*/
--- C/zajecia4/18.c
+++ C/zajecia4/18.c
@@ -2,24 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 int main(){
-    int m = 1;
-    int n = 6;
-
-    int array[9];
-    //osobno parzyste, osobno nieparzyste
-    //5, 3, 1
-    //6, 4, 2
-
+    int m = 1;  // zakres od (oczka kostki)
+    int n = 6;  // zakres do
 
     do {
-
         srand(time(0));
         int liczba = m + (rand() % (n+1-m));
 
         printf("Wylosowano: %i\n", liczba);
-
+        rysuj(liczba);
         char user_input[10];
         printf("k+enter to end\n");
         scanf("%c", user_input);
@@ -27,18 +19,69 @@ int main(){
         if (user_input[0] == 'k'){
             break;
         }
-
     } while (1);
-
-    int arr[6] = {218, 191, 192, 217, 196, 179};
-
-    for (int x=0; x<6; x++) {
-        printf("%c ", arr[x]);
-    }
 
     return 0;
 }
 
+
+void rysuj(int liczba){
+    //int ascii[6] = {218, 191, 192, 217, 196, 179};
+
+    int kostka[25] = {218, 196, 196, 196, 191,
+                      179, 32, 32, 32, 179,
+                      179, 32, 32, 32, 179,
+                      179, 32, 32, 32, 179,
+                      192, 196, 196, 196, 217};
+
+    int parzyste[9] = {42, 32, 42,
+                       42, 32, 42,
+                       42, 32, 42};
+
+    int nieparzyste[9] = {42, 32, 42,
+                          32, 42, 32,
+                          42, 32, 42};
+
+    int licznik = 0;
+    for(int x=0; x<25; x++){
+        licznik++;
+        printf("%c", kostka[x]);
+
+        if (licznik == 5){
+            printf("\n");
+            licznik = 0;
+        }
+    }
+
+
+    for(int x=0; x<5; x++){
+        printf("%c", kostka[x]);
+    }
+    printf("\n");
+    if (liczba % 2 == 0){
+        for(int x=0; x<9; x++){
+        printf("%c", parzyste[x]);
+        }
+    }
+    if (liczba % 2 == 1){
+        for(int x=0; x<9; x++){
+        printf("%c", nieparzyste[x]);
+        }
+    printf("\n");
+    }
+    for(int x=20; x<25; x++){
+        printf("%c", kostka[x]);
+    }
+
+    /*
+    printf("%c%c%c%c%c\n", ascii[0], ascii[4], ascii[4], ascii[4], ascii[1]);
+    printf("%c   %c\n", ascii[5], ascii[5]);
+    printf("%c * %c\n", ascii[5], ascii[5]);
+    printf("%c   %c\n", ascii[5], ascii[5]);
+    printf("%c%c%c%c%c\n\n", ascii[2], ascii[4], ascii[4], ascii[4], ascii[3]);
+    */
+
+}
 
 //          *           *           * *         * *      * *
 //  *                    *                       *       * *
